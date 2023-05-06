43daf6616c2f6630acf7c8643b5b12834d54884c
Techatrix <19954306+Techatrix@users.noreply.github.com>
2023-05-06 23:19:36 +0200

    enable test for lazy analysis on top-level scope

--- tests/sema/identifiers.zig
+++ tests/sema/identifiers.zig
@@ -1,10 +1,10 @@
 const T: type = u32;
-//    ^ (type)(u32);
+//    ^ (type)(u32)
 const OptionalT = ?T;
-//    ^^^^^^^^^ (type)(?u32);
+//    ^^^^^^^^^ (type)(?u32)
 
-// TODO
-// const A = B;
-// //    ^ (type)(u32);
-// const B = u32;
-// //    ^ (type)(u32);
+// TODO lazy analysis for top-level declarations
+const A = B;
+//    ^ ()()
+const B = u32;
+//    ^ (type)(u32)
